e8caf73e369f8af7a70774087d55869ef0ee3e15
ekaeoq <ekaeoq@gmail.com>
2021-10-23 23:07:59 +0200

    maping function

--- App.js
+++ App.js
@@ -3,8 +3,8 @@ import { Text, TextInput, Button, View, KeyboardAvoidingView, StyleSheet } from 
 import Svip from './extra/Svip';
 
 const Joj = () => {
-	const [text, setText] = useState('');
-	const [items, setItems] = useState([]);
+	const [text, setText] = useState('')
+	const [items, setItems] = useState([])
 
 	const addItem = () => {
 		setItems([... items, {
@@ -12,6 +12,12 @@ const Joj = () => {
 			value: Math.floor(Math.random() * 10) + 1
 		}])
 	}
+	const arrayTest = [
+		{id: 1, name: "nekaj", penis: "jao"},
+		{id: 2, name: "bog", nijepenis: "auuuu"}
+	];
+	const arr = ["jao","bok","tko si ti"];
+
   return (
 	  <View style={style.jao}>
 	  	<Text style={style.prvo}>Hello</Text>
@@ -41,6 +47,18 @@ const Joj = () => {
 	  		onPress={addItem}
 	  		title="bok"
 	  	/>
+	  	<View>
+	  		{arr.map(function(name,index){
+				return <Text key={index}>{name}</Text>
+
+			})}
+	  		{arrayTest.map(function(hana){
+				return <Text key={hana.id===1}> {hana.name} </Text>
+			})}
+	  		
+
+			
+	  	</View>
 	  </View>
   );
 }
--- extra/Svip.js
+++ extra/Svip.js
@@ -11,3 +11,4 @@ const Svip = () => {
 };
 
 export default Svip;
+
