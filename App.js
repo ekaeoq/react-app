f2d918933e2077a45c92c87c409759f8a00ffe7f
ekaeoq <ekaeoq@gmail.com>
2021-10-28 05:21:07 +0200

    cleaned up shit code

--- App.js
+++ App.js
@@ -3,36 +3,11 @@ import { Text, FlatList, TextInput, Button, View, KeyboardAvoidingView, StyleShe
 import Svip from './extra/Svip';
 
 const Joj = () => {
-	const [todos, setTodos] = useState([]);
-	const [textInput, setTextInput] = useState('');
-
-
-	const [text, setText] = useState('')
-	const [items, setItems] = useState([])
-
-	const addItem = () => {
-		setItems([... items, {
-			id: items.length,
-			value: Math.floor(Math.random() * 10) + 1
-		}])
-	}
 	const arrayTest = [
 		{id: 1, name: "nekaj", penis: "jao"},
 		{id: 2, name: "bog", nijepenis: "auuuu"}
 	];
 	const arr = ["jao","bok","tko si ti"];
-
-	const addTodo = () => {
-		const newTodo={
-			id:Math.random(),
-			task: textInput,
-			completed: false,
-
-		};
-		setTodos([...todos, newTodo]);
-		setTextInput('');
-	}
-		
 
 
   return (
@@ -42,18 +17,10 @@ const Joj = () => {
 	  	<TextInput
 	  		style={style.textInputStyle}
 	  		placeholder="Type"
-	  		value={textInput}
-	  		onChangeText={(text)=>setTextInput(text)}
-	  		defaultValue={text}
 	  	/>
-	  	<Text style={style.addOnTextStyle}>
-	  		Brate mili jel ovo radi: {text}
-	  	</Text>
 	  	<View>
 	  		<Svip/>
 	  	</View>
-
-	  	
 	  	<KeyboardAvoidingView
 	  		behaviour={Platform.OS === "ios" ? "padding" : "height"}
 	  		style={style.Testing}>
@@ -62,17 +29,8 @@ const Joj = () => {
 
 	  	</KeyboardAvoidingView>
 	  	<Button 
-	  		onPress={addTodo}
 	  		title="bok"
 	  	/>
-	  	<View>
-	  		{items.map(function(name,index){
-				return <Text key={index}>{name}</Text>
-
-			})}
-
-			
-	  	</View>
 	  </View>
   );
 }
